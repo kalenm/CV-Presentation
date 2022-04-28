760b809b78ec24438d4189a0f0bda3ecee2c2c2b
Kalen Mullin <KalenLMullin@gmail.com>
2022-04-28 10:30:46 -0400

    Add files via upload

---
+++ k-means.py
@@ -0,0 +1,45 @@
+# A simple demo of k-means clustering used in photo editing
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Creates list of images to process along with an output list to draw them later
+pics = [cv2.imread('Python\pictures\zune.jpg'),cv2.imread('Python\pictures\gun.jpg'), cv2.imread('Python\pictures\pip.jpg'), cv2.imread('Python\pictures\clouds.jpeg')]
+output = []
+
+plt.rcParams["figure.figsize"] = (14,14)
+final = plt.figure()
+
+
+# Bulk of the image processing happens in here
+for image in pics:
+
+    # Change image to rgb format for better processing
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Converts the image into a 1x2 and converts 
+    pixel_values = image.reshape((-1,3))
+    pixel_values = np.float32(pixel_values)
+
+    # This is the fun stuff
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+    k = 4
+    _, labels, (centers) = cv2.kmeans(pixel_values, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+    # Convert the image colors to the calculated centroids and remake the image, then finally add it to the output list
+    centers = np.uint8(centers)
+    labels = labels.flatten()
+    segmented_image = centers[labels.flatten()]
+    segmented_image = segmented_image.reshape(image.shape)
+    output.append(segmented_image)
+
+
+#allows for multiple images to be drawn on a single figure - this requires that the code only uses 4 images at a time, not gonna bother fixing it
+for i in range(len(output)):
+    final.add_subplot(221 + i)
+    plt.axis('off')
+    plt.imshow(output[i])
+
+
+plt.show()
---
+++ object_detection.py
@@ -0,0 +1,52 @@
+import cv2
+from cv2 import RETR_TREE
+import numpy as np
+
+cap = cv2.VideoCapture(2)
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+kernel = np.ones((7,7),np.uint8)
+
+while(1):
+
+    # Take each frame
+    _, frame = cap.read()
+
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsvInvert = cv2.bitwise_not(hsv)
+
+    # define range of pink color in an inverted HSV
+
+    lower_red = np.array([85, 80, 80])
+    upper_red = np.array([145, 200, 200])
+
+    # Threshold the HSV image to get only pink colors
+    mask = cv2.inRange(hsvInvert, lower_red, upper_red)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+
+    #finding and sorting the countours of the area
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+    if contours:
+        (xMin, yMin, boxWidth, boxHeight) = cv2.boundingRect(contours[0])
+
+        cv2.rectangle(frame, (xMin - 15, yMin - 15), (xMin + boxWidth + 15, yMin + boxHeight + 15), (0, 0, 0), 4)
+
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    
+
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    cv2.imshow('hsv', hsvInvert)
+    
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
